3a8d50e0a88b5073bfcd3f7d6396dc8b3263e75e
Kaiming Kuang <kaiming.kuang@dianei-ai.com>
2020-11-07 11:48:53 +0800

    Restructure agent.

--- my_agent.py
+++ my_agent.py
@@ -1,21 +1,39 @@
 import numpy as np
 from kaggle_environments.envs.rps.utils import get_score
-from scipy.special import expit, softmax
 
 
-class MarkovMomentum:
+class BaseAgent:
 
-    def __init__(self, momentum, mem_len, decision="hard"):
-        self.momentum = momentum        # memory momentum
-        self.mem_len = mem_len          # memory length
-        self.decision = decision
-        self.markov_chain = {}          # markov chain status graph
-        self.last_move = None           # agent's last move
-        self.cur_memory = []            # current memory
+    def update_history(self, self_move, oppo_move):
+        raise NotImplementedError
 
+    def decide(self, obs, cfg):
+        raise NotImplementedError
+
+    def act(self, obs, cfg):
+        raise NotImplementedError
+
+
+class MarkovMomentum(BaseAgent):
+
+    def __init__(self, momentum, mem_len):
+        self.momentum = momentum
+        self.mem_len = mem_len
+        self.history = []
+        self.markov_chain = {}
+        self.last_move = None
+    
     @staticmethod
     def _make_markov_key(cur_memory):
         return ",".join([repr(x) for x in cur_memory])
+    
+    def update_history(self, self_move, oppo_move):
+        if oppo_move is not None:
+            self.history.append((self_move, oppo_move))
+
+        if len(self.history) <= self.mem_len:
+            self._update_markov_chain(self._make_markov_key(self.history[:-1]),
+                oppo_move)
 
     def _update_markov_chain(self, cur_memory_key, oppo_last_move):
         if self.markov_chain.get(cur_memory_key) is None:
@@ -46,109 +64,35 @@ class MarkovMomentum:
             else:
                 pass
 
-        if self.decision == "hard":
-            if len(max_act) > 1:
-                return int((np.random.choice(max_act) + 1) % 3)
-            else:
-                return int((max_act[0] + 1) % 3)
-        elif self.decision == "soft":
-            cnts = np.array(list(self.markov_chain[cur_memory].values()))
-            probs = cnts / cnts.sum()
-            return int((np.random.choice(
-                list(self.markov_chain[cur_memory].keys()), p=probs) + 1) % 3)
+        if len(max_act) > 1:
+            return int((np.random.choice(max_act) + 1) % 3)
         else:
-            cnts = np.array(list(self.markov_chain[cur_memory].values()))
-            probs = softmax(cnts / cnts.sum())
-            return int((np.random.choice(
-                list(self.markov_chain[cur_memory].keys()), p=probs) + 1) % 3)
-
-    def act(self, obs, cfg):
+            return int((max_act[0] + 1) % 3)
+    
+    def decide(self, obs, cfg):
         oppo_last_move = obs.get("lastOpponentAction")
         # act randomly if it's the first round
         if oppo_last_move is None:
             self.last_move = np.random.randint(0, 3)
             return self.last_move
 
-        # add the latest update to the memory
-        self.cur_memory.append((self.last_move, oppo_last_move))
-
-        # if the current memory is too short, act randomly
-        if len(self.cur_memory) <= self.mem_len:
+        # if the current history is too short, act randomly
+        if len(self.history) <= self.mem_len:
             self.last_move = np.random.randint(0, 3)
             return self.last_move
 
-        # update markov chain
-        self._update_markov_chain(self._make_markov_key(self.cur_memory[:-1]),
-            oppo_last_move)
-
-        # pop the earliest memory
-        self.cur_memory.pop(0)
-
         # search for appropriate act in the markov chain
-        self.last_move = self._search_markov_chain(self._make_markov_key(
-            self.cur_memory))
+        decision = self._search_markov_chain(self._make_markov_key(
+            self.history[-2:]))
 
-        return self.last_move
+        return decision
 
-
-class MarkovDynamicMomentum(MarkovMomentum):
-
-    def __init__(self, init_momentum, min_momentum, max_momentum, mem_len):
-        self.momentum = init_momentum
-        self.min_momentum = min_momentum
-        self.max_momentum = max_momentum
-        self.mem_len = mem_len
-        self.markov_chain = {}
-        self.last_move = None
-        self.cur_memory = []
-        self.match_hist = []
-    
-    def _get_latest_avg_score(self, num_games):
-        return np.mean(self.match_hist[-num_games:])
-    
-    def _update_momentum(self):
-        if len(self.match_hist) >= 10:
-            latest_avg_score = self._get_latest_avg_score(10)
-            self.momentum = self.min_momentum\
-                + expit(latest_avg_score * 5)\
-                * (self.max_momentum - self.min_momentum)
-    
-    def _update_match_hist(self, match):
-        self.match_hist.append(get_score(match[0], match[1]))
-    
     def act(self, obs, cfg):
-        oppo_last_move = obs.get("lastOpponentAction")
-        # act randomly if it's the first round
-        if oppo_last_move is None:
-            self.last_move = np.random.randint(0, 3)
-            return self.last_move
-
-        # add the latest update to the memory
-        self.cur_memory.append((self.last_move, oppo_last_move))
-
-        # update match history
-        self._update_match_hist((self.last_move, oppo_last_move))
-
-        # update memory momentum
-        self._update_momentum()
-
-        # if the current memory is too short, act randomly
-        if len(self.cur_memory) <= self.mem_len:
-            self.last_move = np.random.randint(0, 3)
-            return self.last_move
-
-        # update markov chain
-        self._update_markov_chain(self._make_markov_key(self.cur_memory[:-1]),
-            oppo_last_move)
-
-        # pop the earliest memory
-        self.cur_memory.pop(0)
-
-        # search for appropriate act in the markov chain
-        self.last_move = self._search_markov_chain(self._make_markov_key(
-            self.cur_memory))
+        self.update_history(self.last_move, obs.get("lastOpponentAction"))
+        self.last_move = self.decide(obs, cfg)
 
         return self.last_move
+
 
 my_agent = MarkovMomentum(0.5, 2)
 
