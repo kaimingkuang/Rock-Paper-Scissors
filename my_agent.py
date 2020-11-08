d1e98d7e773eda9fd12bbc90102ee5bfaa4d9f8b
Kaiming Kuang <kaiming.kuang@dianei-ai.com>
2020-11-08 14:19:50 +0800

    Added YouGoFirst agent.

--- my_agent.py
+++ my_agent.py
@@ -1,8 +1,24 @@
 import numpy as np
-from kaggle_environments.envs.rps.utils import get_score
+
+
+def get_score(x, y):
+    score = np.where((x - y) % 2 != 0, x - y, np.sign(y - x))
+    
+    return score
+
+
+def kl_divergence(p0, p1):
+    return -p0 * np.log(p1 / (p0 + 1e-8))
+
+
+def symmetric_cross_entropy(p0, p1):
+    return (kl_divergence(p0, p1) + kl_divergence(p1, p0)).mean()
 
 
 class BaseAgent:
+
+    def __init__(self, *args, **kwargs):
+        self.history = []
 
     def update_history(self, self_move, oppo_move):
         raise NotImplementedError
@@ -13,6 +29,15 @@ class BaseAgent:
     def act(self, obs, cfg):
         raise NotImplementedError
 
+    def cal_latest_score(self, hist_len):
+        if len(self.history) == 0:
+            return 0
+
+        score = get_score(np.array([x[0] for x in self.history[-hist_len:]]),
+            np.array([x[1] for x in self.history[-hist_len:]])).sum()
+
+        return score
+
 
 class MarkovMomentum(BaseAgent):
 
@@ -22,7 +47,7 @@ class MarkovMomentum(BaseAgent):
         self.history = []
         self.markov_chain = {}
         self.last_move = None
-    
+
     @staticmethod
     def _make_markov_key(cur_memory):
         return ",".join([repr(x) for x in cur_memory])
@@ -94,7 +119,120 @@ class MarkovMomentum(BaseAgent):
         return self.last_move
 
 
-my_agent = MarkovMomentum(0.5, 2)
+class RandomAgent(BaseAgent):
+
+    def __init__(self, mc_times=100):
+        self.history = []
+        self.mc_times = mc_times
+        self.last_move = None
+
+    def update_history(self, self_move, oppo_move):
+        if oppo_move is not None:
+            self.history.append((self_move, oppo_move))
+
+    def decide(self, obs, cfg):
+        return np.random.randint(0, 3)
+
+    def act(self, obs, cfg):
+        self.update_history(self.last_move, obs.get("lastOpponentAction"))
+        self.last_move = self.decide(obs, cfg)
+
+        return self.last_move
+
+    def cal_latest_score(self, hist_len):
+        if len(self.history) == 0:
+            return 0
+
+        hist_len = min(hist_len, len(self.history))
+        oppo_plays = np.tile(np.array(self.history[-hist_len:])[:, 1],
+            (self.mc_times, 1))
+        rnd_plays = np.random.randint(0, 3, (self.mc_times, hist_len))
+        score = get_score(rnd_plays, oppo_plays).sum(axis=1).mean()
+
+        return score
+
+
+class MetaAgent(BaseAgent):
+
+    def __init__(self, agents, recent_hist_len):
+        self.agents = agents
+        self.recent_hist_len = recent_hist_len
+        self.last_move = None
+
+    def update_history(self, self_move, oppo_move):
+        for i in range(len(self.agents)):
+            self.agents[i].update_history(self.agents[i].last_move, oppo_move)
+
+    def decide(self, obs, cfg):
+        for i in range(len(self.agents)):
+            self.agents[i].last_move = self.agents[i].decide(obs, cfg)
+
+        recent_scores = [self.agents[i].cal_latest_score(self.recent_hist_len)
+            for i in range(len(self.agents))]
+        cur_best_agent = self.agents[np.argmax(recent_scores)]
+
+        return cur_best_agent.last_move
+
+    def act(self, obs, cfg):
+        self.update_history(self.last_move, obs.get("lastOpponentAction"))
+        self.last_move = self.decide(obs, cfg)
+
+        return self.last_move
+
+
+class YouGoFirst(BaseAgent):
+
+    def __init__(self, momentum, sce_thresh, t_wait):
+        self.momentum = momentum 
+        self.sce_thresh = sce_thresh
+        self.t_wait = t_wait
+        self.history = []
+        self.last_move = None
+        self.markov_matrix = np.zeros((3, 3, 3))
+        self.rand_markov_matrix = np.ones(3) / 3
+    
+    def _update_markov_matrix(self):
+        self.markov_matrix[self.history[-2][0], self.history[-2][1]] =\
+            self.markov_matrix[self.history[-2][0], self.history[-2][1]]\
+            * self.momentum
+
+        self.markov_matrix[
+            self.history[-2][0],
+            self.history[-2][1],
+            self.history[-1][1]
+        ] += (1 - self.momentum)
+
+    def update_history(self, self_move, oppo_move):
+        if oppo_move is not None:
+            self.history.append((self_move, oppo_move))
+
+        if len(self.history) >= 2:
+            self._update_markov_matrix()
+
+    def decide(self, obs, cfg):
+        if len(self.history) >= self.t_wait:
+            cond_prob = self.markov_matrix[
+                self.history[-1][0],
+                self.history[-1][1]
+            ]
+            cond_prob /= cond_prob.sum()
+            sce = symmetric_cross_entropy(cond_prob, self.rand_markov_matrix)
+            if sce >= self.sce_thresh:
+                return int((np.argmax(cond_prob) + 1) % 3)
+            else:
+                return np.random.randint(0, 3)
+        else:
+            return np.random.randint(0, 3)
+
+    def act(self, obs, cfg):
+        self.update_history(self.last_move, obs.get("lastOpponentAction"))
+        self.last_move = self.decide(obs, cfg)
+
+        return self.last_move
+
+
+# my_agent = MetaAgent([RandomAgent(), MarkovMomentum(0.5, 2)], 20)
+my_agent = YouGoFirst(0.5, 0.2, 100)
 
 
 def play_rps(observation, configuration):
